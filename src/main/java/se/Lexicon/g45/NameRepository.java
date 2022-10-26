a441b268f3d722cae16c7f0444c84ef5b36b2953
Exilic <tobias.engberg@exilic.se>
2022-10-26 11:07:24 +0200

    Added repository with methods
    NOTE: return values are temporary

---
+++ src/main/java/se/Lexicon/g45/NameRepository.java
@@ -0,0 +1,53 @@
+package se.Lexicon.g45;
+
+public class NameRepository {
+
+    public static int getSize(){
+
+        return 1;
+    }
+
+    public static void setNames(String[] names){
+
+    }
+
+    public static void clear(){
+
+    }
+
+    public static String[] findAll(){
+
+        return new String[0];
+    }
+
+    public static String find(final String fullName){
+
+        return "temporary";
+    }
+
+    public static boolean add(final String fullName){
+
+        return false;
+    }
+
+    public static String[] findByFirstName(final String firstName){
+
+        return new String[0];
+    }
+
+    public static String[] findByLastName(final String lastName){
+
+        return new String[0];
+    }
+
+    public static boolean update(final String original, final String updatedName){
+
+        return false;
+    }
+
+    public static boolean remove(final String fullName){
+
+        return false;
+    }
+
+}
