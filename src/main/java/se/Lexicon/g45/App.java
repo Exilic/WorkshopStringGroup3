8912ac0bf12aef4e83c1f815f06ce5796142db5d
Mattias Bjoerk <mattias.bjork.id1@gmail.com>
2022-10-26 12:52:43 +0200

    added App class

--- src/main/java/se/Lexicon/g45/App.java
+++ src/main/java/se/Lexicon/g45/App.java
@@ -1,8 +1,38 @@
 package se.Lexicon.g45;
 
+
 public class App {
 
     public static void main(String[] args) {
 
+        while (true) {
+
+            menuOptions();
+            // add some switch here perhaps
+            break;
+        }
+
+
     } // main
+
+    /** Prints the main menu options */
+    public static void menuOptions() {
+        System.out.println("********************************************");
+        System.out.println("** Group 45 string management service 1.0 **");
+        System.out.println("********************************************");
+        System.out.println("** (1) Add a new name to the array        **"); // add real option things
+        System.out.println("** (2) Add a new name to the array        **");
+        System.out.println("** (3) Add a new name to the array        **");
+        System.out.println("** (4) Add a new name to the array        **");
+        System.out.println("** (5) Add a new name to the array        **");
+        System.out.println("** (6) Add a new name to the array        **");
+        System.out.println("** (7) Add a new name to the array        **");
+        System.out.println("** (8) Add a new name to the array        **");
+        System.out.println("** (9) Add a new name to the array        **");
+        System.out.println("** (10) Add a new name to the array       **");
+        System.out.println("** (11) Add a new name to the array       **");
+        System.out.println("********************************************");
+        System.out.print("Option: ");
+    }
+
 } // App
